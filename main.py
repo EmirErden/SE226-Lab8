a2acf9501feb501f6836ecc4e753b965f7edcd6f
EmirErden <103561700+EmirErden@users.noreply.github.com>
2022-06-01 22:56:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,45 @@
 from abc import ABC
 
+class Count(ABC):
+    def __init__(self, address):
+        self.address = address
 
-class adress(ABC):
-    def _init_(self, address):
-        self.address = "C:\\Users\\C605\\Desktop\\strange.txt"
-
-    def calculateFreqs(self):
+    def calculateFreqs(self, address):
         pass
 
+class ListCount(Count):
+    def __init__(self, address):
+        Count.__init__(self, address)
 
-class ListCount(adress):
-    def calculateFreqs(self):
-        file = open()
+    def calculateFreqs(self, address):
+        file = open(address)
+        list = file.readline().split()
+        words = []
+
+        for i in list:
+            if i not in words:
+                words.append(i)
+        for a in range(0, len(words)):
+            print(words[a], "=", list.count(words[a]))
+
+
+class DictCount(Count):
+    def __init__(self, address):
+        Count.__init__(self, address)
+
+    def calculateFreqs(self, address):
+        file = open(address)
+        list = file.readline()
+        wordDict = {}
+        for word in list.split():
+            wordDict[word] = wordDict.get(word, 0) + 1
+        for key in wordDict:
+            print("{} : {}".format(key, wordDict[key]))
+
+
+txtFile = "strange.txt"
+list = ListCount(txtFile)
+list.calculateFreqs(txtFile)
+
+dictionary = DictCount(txtFile)
+dictionary.calculateFreqs(txtFile)
